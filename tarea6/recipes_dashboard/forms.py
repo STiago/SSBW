20e7da938591d6af4909dc05ecff9023bc8b9835
STiago <victoriasantiago@correo.ugr.es>
2018-05-13 16:40:47 +0100

    Cleaning files closed #7

--- tarea6/recipes_dashboard/forms.py
+++ tarea6/recipes_dashboard/forms.py
@@ -2,9 +2,6 @@ from django import forms
 from .models import Recipe
 from mongoengine import *
 
-#from django.forms import ModelForm
-#from mongodbforms import DocumentForm
-
 
 class RecipeForm(forms.Form):
     name    = forms.CharField(label='Photo name', max_length=60,
@@ -18,14 +15,7 @@ class RecipeForm(forms.Form):
 
     def saved(self):
         data = self.cleaned_data
-        print("jaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", data, type(data))
         tags = data['tags'].split(",")
-        #recipe = RecipeForm()
-        #recipe.name=data['name']
-        #recipe.tags=tags
-        #recipe.image=data['image']
-        #recipe = Recipe(name=data['name'], tags=tags, photo_file=data['image'])#data['tags'], photo_file=data['image'])
-        #recipe.save()
         return data['image']
 
     #def saved(self, *args, **kwargs):
@@ -33,9 +23,3 @@ class RecipeForm(forms.Form):
 
     #def __init__(self, *args, **kwargs):
         #super(RecipeForm, self).__init__(*args, **kwargs)
-
-    #def saved(self, request, recipe):
-        #recipe.tags = self.cleaned_data['tags']
-        #recipe.image = self.cleaned_data['image']
-        #recipe.name = self.cleaned_data['name']
-        #recipe.save()
